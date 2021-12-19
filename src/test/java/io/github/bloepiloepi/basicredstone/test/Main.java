21f903386ef19c6fda2f72dd088056a3e91d44f8
LeoDog896 <LeoDog896@hotmail.com>
2021-12-19 10:12:56 -0500

    Fix up build errors

--- src/test/java/io/github/bloepiloepi/basicredstone/test/Main.java
+++
@@ -1,9 +0,0 @@
-package io.github.bloepiloepi.basicredstone.test;
-
-import net.minestom.server.Bootstrap;
-
-public class Main {
-	public static void main(String[] args) {
-		Bootstrap.bootstrap("io.github.bloepiloepi.basicredstone.test.RedstoneTest", args);
-	}
-}
